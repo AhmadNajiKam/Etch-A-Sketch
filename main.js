8f032bc531bceb19709bdc0b978e56fa5eb98175
AhmadNajiKam <ahmadnajikam@gmail.com>
2025-01-30 23:46:00 +0300

    Fix how cells are implemented and add hover functionallity

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+let color = "blue";
 for (let i = 1; i <= 16; i++) {
   const row = document.createElement("div");
   row.classList.add("row");
@@ -10,4 +11,10 @@ for (let i = 1; i <= 16; i++) {
   container.appendChild(row);
 }
 container.classList.add("container");
+const cols = document.querySelectorAll(".column");
+cols.forEach((x) => {
+  x.addEventListener("mouseenter", () => {
+    x.style.backgroundColor = color;
 
+  });
+})
